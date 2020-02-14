33b45e3bef4085e4c2cc316b24f55d5944764553
Sean Dwyergmail.com <spdwyer06@gmail.com>
2020-02-14 10:18:18 -0500

    added prompt

--- 01_Cafe/Program.cs
+++ 01_Cafe/Program.cs
@@ -6,7 +6,27 @@ namespace _01_Cafe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /*
+             Console app that is be able to: create new menu items, delete menu items, and receive a list of all items on the cafe's menu
+
+             The Menu Items:
+                A meal number, so customers can say "I'll have the #5"
+                A meal name
+                A description
+                A list of ingredients,
+                A price
+ 
+
+            Your Task:
+                Create a Menu Class with properties, constructors, and fields.
+                Create a MenuRepository Class that has methods needed.
+                Create a Test Class for your repository methods. (You don't need to test your constructors or objects, just your methods)
+                Create a Program file that allows the cafe manager to add, delete, and see all items in the menu list.
+ 
+
+            Notes:
+                We don't need to be able to update items right now.
+            */
         }
     }
 }
